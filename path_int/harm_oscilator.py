7649f0ef503806b6b34540c3d10d921fd4069d05
jrsteiner <jrsteiner78@gmail.com>
2017-11-30 10:48:19 -0300

    Implementação da variável com o intervalo de integração
    L

--- path_int/harm_oscilator.py
+++ path_int/harm_oscilator.py
@@ -7,7 +7,7 @@ utilizando um algoritmo de cadeia de Markov
 import numpy as np
 
 def en_free(n, L):
-    return 2.n*n(np.pi**2)/(L*L)
+    return 2.*n*n(np.pi**2)/(L*L)
 
 def rho_free(beta, n, L, x1, x2):
     rho = np.zeros(n)
@@ -15,7 +15,7 @@ def rho_free(beta, n, L, x1, x2):
     _temp2 = .0
     for iter in range(n):
         _temp1 = np.exp(2j*np.pi*iter*(x1 - x2)/L)
-        _temp2 = np.exp(-(beta*2.*n*n*(no.pi**2))/(L*L))
+        _temp2 = np.exp(-(beta*2.*n*n*(np.pi**2))/(L*L))
         rho[iter] = _temp1 * _temp2
     return np.sum(rho)
 
@@ -23,12 +23,15 @@ N = 8
 beta = 4
 mc_iter = 10**6
 delta = 0.1
+num_points = 20
 
-x0 = 0.0
+xi = -2.0
 xf = 2.0
 
-path = np.arange(x0, xf, N-1)
-path2 = np.arange(x0, xf, N-1)
+path = np.arange(xi, xf, N-1)
+path2 = np.arange(xi, xf, N-1)
+
+L = np.linspace(xi, xf, num_points)
 
 k = np.random.randint(0, N-1)
 kp = k + 1
@@ -36,7 +39,5 @@ km = k -1
 
 if km == -1:
     km = N
-
-
 
 path2[k] = path[k] + np.random.rand(-delta, delta)
